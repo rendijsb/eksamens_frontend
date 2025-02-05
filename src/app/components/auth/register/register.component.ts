39327f0b74d54342ad19ec6a78685f78885aebbc
rendijsb <132062428+rendijsb@users.noreply.github.com>
2025-02-06 00:37:01 +0200

    Samazinaju autorizacijas formu izmerus un saku stradat pie register endpoint izsauksanu

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ReactiveFormsModule, UntypedFormBuilder, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-register',
-    imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
     templateUrl: './register.component.html',
     styleUrl: '../styles/auth.scss'
 })
 export class RegisterComponent {
+  private readonly formBuilder = inject(UntypedFormBuilder);
 
+  registerForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    password: ['', Validators.required],
+    password_confirmation: ['', Validators.required]
+  });
+
+  onSubmit() {
+
+  }
 }
---
+++ src/app/components/auth/services/auth.service.ts
@@ -0,0 +1,13 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly http = inject(HttpClient);
+
+  register(data: any) {
+    return this.http.post('http://localhost:8000/api/register', data);
+  }
+}
