d580441562f10c4f0103f0e5a69f3b907b79ce4c
rendijsberkis <rendijsberkis7@gmail.com>
2025-05-15 23:50:48 +0300

    pievienoju railway nepieciesamibas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'eksamensbackend-production.up.railway.app',
+  apiUrl: 'https://eksamensbackend-production.up.railway.app',
   stripePublishableKey: 'pk_test_51R5D0G4ERaoA0cXEB3oWlo0id1pPW7uojvInrNtT19TmMMzL4Xc7lRyj6hMnLJWY5nWyHZELhukWa3ePmF9UoU9j00buyKNQsE'
 };
